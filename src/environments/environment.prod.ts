950467db47551a966ba7a51c8101c3639f192127
Ruby Dev <roseruby143@gmail.com>
2023-08-30 14:37:16 -0700

    update envt variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:8081',
+  baseUrl: 'http://ec2-54-82-234-235.compute-1.amazonaws.com:8081',
   projectTitle : 'Eat Out',
   projectTagLine : 'With you, On every bite of your life!'
 };
